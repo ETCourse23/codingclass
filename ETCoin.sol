f1c7abc02e7e82e7852e7c2eb433372e218f786a
ETCourse23 <145016434+ETCourse23@users.noreply.github.com>
2023-10-19 15:03:14 +0200

    Update ETCoin.sol

--- ETCoin.sol
+++ ETCoin.sol
@@ -1,1 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ETCoin is ERC20 {
+    constructor() ERC20("ETCoin", "ETCX") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+}
